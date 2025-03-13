a300deeaf6aa2ef37d2552ddcf452f5721389bee
quangfoden <nguyenvanquang22103@gmail.com>
2025-03-13 17:31:59 +0700

    update code

---
+++ database/migrations/2025_03_13_102728_create_borrow_requests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('borrow_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('device_id')->constrained()->onDelete('cascade');
+            $table->timestamp('borrow_date')->useCurrent();
+            $table->timestamp('return_date')->nullable();
+            $table->enum('status', ['pending', 'approved', 'returned', 'denied'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('borrow_requests');
+    }
+};
